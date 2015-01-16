5a0274b2a0b3dd18f61bf1d63ff1ce0929b457c8
Nikita <tukov@bk.ru>
2015-01-16 17:13:30 +0600

    dz6 started

---
+++ dz6.php
@@ -0,0 +1,51 @@
+
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+        <title>hlama.net</title>
+        <link href="css/style.css" rel="stylesheet" type="text/css">
+    </head>
+    <body>
+        <div id="maket">
+            <div id="header">
+                <h1> HLAMA.NET - лучшая барахолка рунета</h1>
+            </div>
+            <div id="left">Левая колонка</div>
+            <div id="content">
+                <form action="">
+                    <fieldset>
+                        <dl>
+                            <dt><label for="name">Твое имя</label></dt>
+                            <dd><input type="text" id="name" /></dd>
+                            <dt><label for="email">Твой email</label></dt>
+                            <dd><input type="text" id="email" /></dd>
+                            <dt><label for="theme">Тема сообщения</label></dt>
+                            <dd>
+                                <select name="theme" id="theme">
+                                    <option></option>
+                                    <option>Блaгодарность</option>
+                                    <option>Нашел ошибку</option>
+                                    <option>Хочу дополнить</option>
+                                    <option>Есть идея</option>
+                                    <option>Сотрудничество</option>
+                                    <option>С темой не определился</option>
+                                </select>
+                            </dd>
+                            <dt><label for="message">Текст *</label></dt>
+                            <dd><textarea cols="" rows=""  id="message"></textarea></dd>
+                            <dt> </dt>
+                            <dd><input type="submit" name="send" value="отправить"/> </dd>
+                                                        
+                        </dl>
+                        
+                            <!--<input type="submit" name="send" value="отправить" />-->
+                        
+                    </fieldset>
+                </form>
+            
+            </div>
+            <div id="footer">Подвал</div>
+        </div>
+    </body>
+</html>
+
