3076d8d46d97fabc33125f83af73755dd69d6e9e
Nikita <tukov@bk.ru>
2015-03-03 16:54:16 +0600

    install in progress

--- dz9/install.php
+++ dz9/install.php
@@ -2,6 +2,8 @@
 error_reporting(E_ERROR |  E_WARNING | E_PARSE);
 ini_set('display_errors', 1);
 
+//функция для создания файла config.ini где будут хранится настройки доступа к базе. 
+
 function write_ini_file($assoc_arr, $path, $has_sections=FALSE) { 
     $content = ""; 
     if ($has_sections) { 
@@ -43,28 +45,65 @@ function write_ini_file($assoc_arr, $path, $has_sections=FALSE) {
     $success = fwrite($handle, $content);
     fclose($handle); 
 }
-    $sampleData = array(
-                'first' => array(
-                    'first-1' => 1,
-                    'first-2' => 2,
-                    'first-3' => 3,
-                    'first-4' => 4,
-                    'first-5' => 5,
-                ),
-                'second' => array(
-                    'second-1' => 1,
-                    'second-2' => 2,
-                    'second-3' => 3,
-                    'second-4' => 4,
-                    'second-5' => 5,
+//функция удаляет любые таблицы из базы
+function delete_table($db) {
+
+    $query = 'SHOW TABLES FROM `' . $db . '` ';
+    $result = mysqli_query($db, $query);
+
+    //получаем список всех таблиц из указанной базы данных и удаляем их 
+    while ($arr = mysqli_fetch_row($result)) {
+        $arr2[] = $arr[0];
+    }
+    if (!empty($arr2)) {
+        foreach ($arr2 as $value) {
+            $delete .= 'DROP TABLE IF EXISTS `' . $value . '`;';
+        }
+        mysqli_multi_query($db, $delete) or die(mysqli_error($db));
+        while (mysqli_next_result($db)) {;}
+    }
+}
+
+function create_table($db) {
+//создаем базу из дампа
+    $create_table = file_get_contents('hlamanet.sql');
+    mysqli_multi_query($db, $create_table) or die(mysqli_error($db));
+}
+
+if (array_key_exists('send', $_POST)){
+    //сформируем массив для записи в файл
+    $Data = array(
+                'Database1' => array(
+                    'host' => $_POST['host'],
+                    'user' => $_POST['user'],
+                    'password' => $_POST['pass'],
+                    'database' => $_POST['database'],
                 ));
-write_ini_file($sampleData, './config.ini', true);
     
-
-$data = parse_ini_file('./config.ini', true);
-
-var_dump($data);
-
+    //функция записи ini файла
+    write_ini_file($Data, './config.ini', true);
+    print_r($_POST);
+    $db = new mysqli($_POST['host'], $_POST['user'], $_POST['pass'], $_POST['database']) or die (mysqli_error($db));
+//    
+//    delete_table($db);
+//    create_table($db);
+    mysqli_close($db);
+}
 
 
 ?>
+
+<form  id = "dataform" action="install.php" method="POST">
+                                            
+                            <label for="host">Server name</label><br>
+                            <input type="text" name="host" value="" /><br>
+                            <label for="user">User name</label><br>
+                            <input type="text" name="user" value="" /><br>
+                            <label for="pass">Password</label><br>
+                            <input type="password" name="pass" value="" /><br>
+                            <label for="database">Database</label><br>
+                            <input type="text" name="database" value="" /><br>
+                                                        
+                            <input type="submit" name="send" value="Install" />
+
+
