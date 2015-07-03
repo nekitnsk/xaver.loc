cc2cad24e3817f68cca150e906410db6fe770c87
Nikita <tukov@bk.ru>
2015-07-03 16:37:17 +0600

    house project

---
+++ house/index.php
@@ -0,0 +1,308 @@
+<!doctype html>
+<!--[if lt IE 7]> <html class="no-js ie6 oldie" lang="en"> <![endif]-->
+<!--[if IE 7]>    <html class="no-js ie7 oldie" lang="en"> <![endif]-->
+<!--[if IE 8]>    <html class="no-js ie8 oldie" lang="en"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang="en-US"> <!--<![endif]-->
+<head>
+
+<!-- DEFAULT META TAGS -->
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<title>Home | Xone | Clean One Page Template</title>
+<!-- DEFAULT META TAGS -->
+
+<!-- FONTS -->
+<link href='http://fonts.googleapis.com/css?family=Open+Sans:300,400,800,600|Raleway:300,600,900' rel='stylesheet' type='text/css'>
+<!-- FONTS -->
+
+<!-- CSS -->
+<link rel='stylesheet' id='default-style-css'  href='files/css/style.css' type='text/css' media='all' />
+<link rel='stylesheet' id='flexslider-style-css'  href='files/css/flexslider.css' type='text/css' media='all' />
+<link rel='stylesheet' id='easy-opener-style-css'  href='files/css/easy-opener.css' type='text/css' media='all' />
+<link rel='stylesheet' id='jplayer-style-css'  href='files/jplayer/jplayer.css' type='text/css' media='all' />
+<link rel='stylesheet' id='isotope-style-css'  href='files/css/isotope.css' type='text/css' media='all' />
+<link rel="stylesheet" id='rsplugin-style-css' href="files/rs-plugin/css/settings.css" type="text/css" media="all" />
+<link rel="stylesheet" id='fontawesome-style-css' href="files/css/font-awesome.min.css" type="text/css" media="all" />
+<link rel='stylesheet' id='retina-style-css'  href='files/css/retina.css' type='text/css' media='all' />
+<link rel='stylesheet' id='mqueries-style-css'  href='files/css/mqueries.css' type='text/css' media='all' />
+<!-- CSS -->
+
+<!-- FAVICON -->
+<link rel="shortcut icon" href="files/uploads/favicon.png"/>
+<!-- FAVICON -->
+
+<!-- JQUERY LIBRARY & MODERNIZR -->
+<script src="files/js/jquery-1.9.1.min.js"></script>
+<script src='files/js/jquery.modernizr.min.js'></script>
+<!-- JQUERY LIBRARY & MODERNIZR -->
+
+</head>
+
+<body> 
+
+<!-- PAGELOADER -->
+<div id="page-loader">
+	<div class="page-loader-inner">
+    	<div class="loader-logo"><img src="files/uploads/xone-logo.png" alt="Logo"/></div>
+		<div class="loader-icon"><span class="spinner"></span><span></span></div>
+	</div>
+</div>
+<!-- PAGELOADER -->
+
+<!-- PAGE CONTENT -->
+<div id="page-content" class="fixed-header">
+
+	
+    
+	<!-- PAGEBODY -->
+	<div class="page-body">
+    
+		
+
+		
+		<!-- Boxed Layout -->   
+        <div class="wrapper">
+
+          <div class="section-title">
+                    <h2>Каталог домов</h2>
+                    <div class="seperator size-small"><span></span></div>
+                    <h4 class="subtitle">Воспользуйтесь фильтром по категориям</h4>
+                </div>
+          
+          <div class="seperator size-mini height-small"><span></span></div>
+
+
+
+
+          
+          <div class="spacer spacer-small"></div>
+          
+          <ul id="portfolio-filter5" class="filter clearfix" data-related-grid="portfolio-grid5">
+
+
+            <li class = "li_hidden"><a data-option-value=".default" href="#" title="Default">Default</a></li>
+            <li><a data-option-value=".brick" href="#" title="Кирпич">Кирпич</a></li>
+            <li><a data-option-value=".log" href="#" title="Оцилиндрованное бревно">Оцилиндрованное бревно</a></li>
+            <li><a data-option-value=".sandwich" href="#" title="Сэндвич панели">Сэндвич панели</a></li>
+            <li><a data-option-value=".rod" href="#" title="Профилированный брус">Профилированный брус</a></li>
+            <li><a data-option-value=".sibit" href="#" title="Motion">Сибит</a></li>
+            
+          </ul>
+        
+        <div id="portfolio-grid5" class="masonry portfolio-entries portfolio-spaced clearfix" data-maxitemwidth="370">
+        
+          <div class="portfolio-masonry-entry masonry-item brick default">
+                    <div class="entry-intro portfolio-intro">
+                      <div class="intro-headline portfolio-intro-headline">
+                        <h5 class="portfolio-name"><a href="files/page/aero_polar_42.html" class="load-content"><strong>Дом Aero polar 42</strong></a></h5>
+                      </div>
+                    </div>
+                      <div class="entry-thumb portfolio-thumb"> 
+                        <div class="imgoverlay text-light">
+                            <a href="files/page/aero_polar_42.html" class="load-content">
+                                <img src="files/uploads/house/aero_polar_42/aero_polar_42_550х350.jpg" alt="SEO IMAGE NAME" />
+                            <div class="overlay"><span class="overlaycolor"></span><div class="overlayinfo"><h6>Площадь 42 м<sup>2</sup><br>Стоимость от 790 500 руб.</h6></div></div>
+                            </a>
+                        </div>
+                    </div>
+                   
+                      
+                  </div> <!-- END .portfolio-masonry-entry -->
+        
+          <div class="portfolio-masonry-entry masonry-item web default">
+                    <div class="entry-intro portfolio-intro">
+                      <div class="intro-headline portfolio-intro-headline">
+                        <h5 class="portfolio-name"><a href="#" class="load-content"><strong>Дом 2</strong></a></h5>
+                      </div>
+                    </div>
+
+                    <div class="entry-thumb portfolio-thumb">
+                      <div class="imgoverlay text-light">
+                        <a href="#" class="load-content">
+                          <img src="files/uploads/550x350.jpg" alt="SEO IMAGE NAME" />
+                          <div class="overlay"><span class="overlaycolor"></span><div class="overlayinfo"><h6>Дом 2</h6></div></div>
+                        </a>
+                      </div>
+                    </div>
+
+
+                  </div> <!-- END .portfolio-masonry-entry -->
+          
+          <div class="portfolio-masonry-entry masonry-item log default">
+            <div class="entry-intro portfolio-intro">
+                        <div class="intro-headline portfolio-intro-headline">
+                            <h5 class="portfolio-name"><a href="#" class="load-content"><strong>Дом 3</strong></a></h5>
+                        </div>
+                      </div>
+                      <div class="entry-thumb portfolio-thumb">
+                        <div class="imgoverlay text-light">
+                            <a href="#" class="load-content">
+                                <img src="files/uploads/550x350.jpg" alt="SEO IMAGE NAME" />
+                            <div class="overlay"><span class="overlaycolor"></span><div class="overlayinfo"><h6>Дом 3</h6></div></div>
+                            </a>
+                        </div>
+                    </div>
+                   
+                      
+                  </div> <!-- END .portfolio-masonry-entry -->
+          
+          <div class="portfolio-masonry-entry masonry-item sandwich default">
+            <div class="entry-intro portfolio-intro">
+                        <div class="intro-headline portfolio-intro-headline">
+                            <h5 class="portfolio-name"><a href="#" class="load-content"><strong>Дом 4</strong></a></h5>
+                        </div>
+                      </div>
+                      <div class="entry-thumb portfolio-thumb">
+                        <div class="imgoverlay text-light">
+                            <a href="#" class="load-content">
+                                <img src="files/uploads/550x350.jpg" alt="SEO IMAGE NAME" />
+                            <div class="overlay"><span class="overlaycolor"></span><div class="overlayinfo"><h6>Дом 4</h6></div></div>
+                            </a>
+                        </div>
+                    </div>
+                   
+                      
+                  </div> <!-- END .portfolio-masonry-entry -->
+          
+          <div class="portfolio-masonry-entry masonry-item rod default">
+            <div class="entry-intro portfolio-intro">
+                        <div class="intro-headline portfolio-intro-headline">
+                            <h5 class="portfolio-name"><a href="#" class="load-content"><strong>Дом 5</strong></a></h5>
+                        </div>
+                      </div>
+                      <div class="entry-thumb portfolio-thumb">
+                        <div class="imgoverlay text-light">
+                            <a href="#" class="load-content">
+                                <img src="files/uploads/550x350.jpg" alt="SEO IMAGE NAME" />
+                            <div class="overlay"><span class="overlaycolor"></span><div class="overlayinfo"><h6>Дом 5</h6></div></div>
+                            </a>
+                        </div>
+                    </div>
+                   
+                      
+                  </div> <!-- END .portfolio-masonry-entry -->
+        
+          <div class="portfolio-masonry-entry masonry-item sibit default">
+            <div class="entry-intro portfolio-intro">
+                        <div class="intro-headline portfolio-intro-headline">
+                            <h5 class="portfolio-name"><a href="#" class="load-content"><strong>Дом 6</strong></a></h5>
+                        </div>
+                      </div>
+                      <div class="entry-thumb portfolio-thumb">
+                        <div class="imgoverlay text-light">
+                            <a href="#" class="load-content">
+                                <img src="files/uploads/550x350.jpg" alt="SEO IMAGE NAME" />
+                            <div class="overlay"><span class="overlaycolor"></span><div class="overlayinfo"><h6>Дом 6</h6></div></div>
+                            </a>
+                        </div>
+                    </div>
+                   
+                      
+                  </div> <!-- END .portfolio-masonry-entry -->
+          
+          <div class="portfolio-masonry-entry masonry-item brick default">
+             <div class="entry-intro portfolio-intro">
+                        <div class="intro-headline portfolio-intro-headline">
+                            <h5 class="portfolio-name"><a href="#" class="load-content"><strong>Дом 7</strong></a></h5>
+                        </div>
+                      </div>
+                      <div class="entry-thumb portfolio-thumb">
+                        <div class="imgoverlay text-light">
+                            <a href="#" class="load-content">
+                                <img src="files/uploads/550x350.jpg" alt="SEO IMAGE NAME" />
+                            <div class="overlay"><span class="overlaycolor"></span><div class="overlayinfo"><h6>Дом 7</h6></div></div>
+                            </a>
+                        </div>
+                    </div>
+                   
+                     
+                  </div> <!-- END .portfolio-masonry-entry -->
+          
+          <div class="portfolio-masonry-entry masonry-item sandwich default">
+            <div class="entry-intro portfolio-intro">
+                        <div class="intro-headline portfolio-intro-headline">
+                            <h5 class="portfolio-name"><a href="#" class="load-content"><strong>Дом 8</strong></a></h5>
+                        </div>
+                      </div>
+                      <div class="entry-thumb portfolio-thumb">
+                        <div class="imgoverlay text-light">
+                            <a href="#" class="load-content">
+                                <img src="files/uploads/550x350.jpg" alt="SEO IMAGE NAME" />
+                            <div class="overlay"><span class="overlaycolor"></span><div class="overlayinfo"><h6>Дом 8</h6></div></div>
+                            </a>
+                        </div>
+                    </div>
+                   
+                      
+                  </div> <!-- END .portfolio-masonry-entry -->
+        
+        </div> <!-- END #portfolio-grid1 -->
+        </div> <!-- END .wrapper --> 
+        <!-- Boxed Layout -->
+
+           <!-- AJAX AREA -->    
+           <div id="ajax-portfolio" class="ajax-section">
+            <div id="ajax-loader"><div class="loader-icon"><span class="spinner"></span><span></span></div></div>
+            <div class="ajax-content clearfix"> 
+              <!-- THE LOADED CONTENT WILL BE ADDED HERE -->
+            </div>
+            <div class="close-project"><a href="index.html">Close</a></div>
+          </div>    
+          <!-- AJAX AREA -->   
+
+
+
+         	<div class="spacer spacer-big"></div>   
+           
+		   	
+        
+      	<!-- FOOTER -->  
+		<footer>
+			<div class="footerinner wrapper align-center text-light">
+				<a id="backtotop" href="#" class="sr-button sr-buttonicon small-iconbutton"><i class="fa fa-angle-up"></i></a>
+				<p class="footer-logo"><img src="files/uploads/xone-logo-footer.png" alt="Footer Logo"></p>
+             	<ul class="socialmedia-widget social-share">
+ 					<li class="facebook"><a href="#">Facebook</a></li>
+  					<li class="twitter"><a href="#">Tweet</a></li>
+ 					<li class="linkedin"><a href="#">Google Plus</a></li>
+ 					<li class="dribbble"><a href="#">Dribble</a></li>
+ 					<li class="behance"><a href="#">Behance</a></li>
+ 					<li class="instagram"><a href="#">Instagram</a></li>
+            	</ul>
+            	<p class="copyright">Copyright &copy; 2014 - Xone - Created by SpabRice</p>
+         	</div>
+    	</footer>
+      	<!-- FOOTER -->         
+        
+ 	</div> <!-- END .page-body -->
+	<!-- PAGEBODY -->
+    
+</div> <!-- END #page-content -->
+<!-- PAGE CONTENT -->
+
+
+<!-- SCRIPTS -->
+<script type='text/javascript' src='files/js/retina.js'></script>
+<script type='text/javascript' src='files/js/jquery.easing.1.3.js'></script>
+<script type='text/javascript' src='files/js/jquery.easing.compatibility.js'></script>
+<script type='text/javascript' src='files/js/jquery.visible.min.js'></script>
+<script type='text/javascript' src='files/js/jquery.easy-opener.min.js'></script>
+<script type='text/javascript' src='files/js/jquery.flexslider.min.js'></script>
+<script type='text/javascript' src='files/js/jquery.isotope.min.js'></script>
+<script type='text/javascript' src='files/js/jquery.bgvideo.min.js'></script>
+<script type='text/javascript' src='files/js/jquery.fitvids.min.js'></script>
+<script type='text/javascript' src='files/jplayer/jquery.jplayer.min.js'></script>
+<script type="text/javascript" src="files/rs-plugin/js/jquery.themepunch.plugins.min.js"></script>
+<script type="text/javascript" src="files/rs-plugin/js/jquery.themepunch.revolution.min.js"></script>
+<script type='text/javascript' src='files/js/jquery.parallax.min.js'></script>
+<script type='text/javascript' src='files/js/jquery.counter.min.js'></script>
+<script type='text/javascript' src='files/js/jquery.scroll.min.js'></script>
+<script type='text/javascript' src='files/js/xone-header.js'></script>
+<script type='text/javascript' src='files/js/xone-loader.js'></script>
+<script type='text/javascript' src='files/js/xone-form.js'></script>
+<script type='text/javascript' src='files/js/script.js'></script>
+<!-- SCRIPTS -->
+
+</body>
+</html>
